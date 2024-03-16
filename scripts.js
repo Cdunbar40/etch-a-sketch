b5b6720c5bda83b0759597edc13a5f01067922b4
Cdunbar40 <cdunbar1240@gmail.com>
2024-03-16 13:47:56 -0400

    Added initial sketch feature and the ability to reset and resize the grid.

--- scripts.js
+++ scripts.js
@@ -1,5 +1,10 @@
 const body = document.querySelector('body');
-body.setAttribute('style','display:flex; justify-content:center;align-items:center;');
+body.setAttribute('style','display:flex; justify-content:center;align-items:center; flex-direction:column;');
+const resetBtn = document.createElement('button');
+resetBtn.textContent = 'Reset';
+resetBtn.addEventListener('click', resetGrid);
+body.appendChild(resetBtn);
+
 const sheet = document.createElement('div');
 sheet.setAttribute("style",`background-color: lightgrey;
                     width: 960px;
@@ -18,12 +23,43 @@ for (var i = 0; i < 16*16; i++){
                             flex: 0 0 auto;`);
     pix.classList.add('pixel');
     pix.addEventListener(
-        "mouseenter",
+        "mouseover",
         (event) => {
-          event.target.setAttribute('style', 'background-color:orange;');
-          console.log('fired'); });
+          event.target.style.backgroundColor='black';
+        });
     sheet.appendChild(pix);
 }
 
-
 body.appendChild(sheet);
+
+function resetGrid() {
+    var size = prompt('Please enter your desired grid size (max: 100):');
+    
+    if(size === null){
+        return;
+    }
+    if(!parseInt(size)){
+        size = 16;
+    }
+    else if(size > 100){
+        size = 100;
+    }
+    console.log(size);
+    sheet.innerHTML = '';
+    var dim = 960 / size;
+    for (var i = 0; i < size * size; i++){
+        const pix = document.createElement('div');
+        pix.setAttribute('style', `width:${dim}px;
+                                height:${dim}px;
+                                border:0px; 
+                                margin:0px;
+                                flex: 0 0 auto;`);
+        pix.classList.add('pixel');
+        pix.addEventListener(
+            "mouseover",
+            (event) => {
+            event.target.style.backgroundColor='black';
+            });
+        sheet.appendChild(pix);
+    }
+}
